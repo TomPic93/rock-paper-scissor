826a5c94d8022923cc72c9139de36e5462a59bce
Tommaso Piccioni <tommaso.piccioni.cr@gmail.com>
2022-10-25 12:39:53 -0600

    Adds html, basic stylesheet and script with game's logical structure

---
+++ script.js
@@ -0,0 +1,46 @@
+// TODO LIST
+
+
+// GLOBAL SELECTORS 
+
+const player = document.querySelector("#playerName");
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector("#computerScore");
+const playerChoiceDisplay = document.querySelector("#playerChoiceDisplay");
+const computerChoiceDisplay = document.querySelector("#computerChoiceDisplay");
+const symbols = document.querySelectorAll(".symbol")
+const rockSymbol = document.querySelector("#rock");
+const paperSymbol = document.querySelector("#paper");
+const scissorSymbol = document.querySelector("#scissor");
+const roundChoicesDisplay = document.querySelector("#roundChoicesDisplay");
+const roundWinnerDisplay = document.querySelectorAll("#roundWinnerDisplay");
+const playAgainDisplay = document.querySelector("#playAgainDisplay");
+const playAgainBtn = document.querySelector("#playAgainBtn");
+
+
+// GAME FUNCTIONS
+
+// sets the initial values and coordinates the other functions
+function startGame() {
+    
+}
+
+// let the user select his/her choice
+function userSelection() {
+
+}
+
+// random choice 
+function computerSelection() {
+
+}
+
+// compares the user and computer choices and determines the winner
+function compareChoices() {
+
+}
+
+// called when the game reach the maximum score allowed
+function gameOver() {
+    
+}
